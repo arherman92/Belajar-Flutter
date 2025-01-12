1b17a98968d08a409616730866f770f80d6f0173
arherman92 <117330392+arherman92@users.noreply.github.com>
2025-01-12 20:25:06 +0800

    menerapkan code
    saya membuat code layout mengunakan  Scaffold AppBar dan Drawer

--- flutter_layout/lib/basic_screen.dart
+++ flutter_layout/lib/basic_screen.dart
@@ -6,11 +6,32 @@ class BasicScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 99, 209, 77),
+        title: const Text('Welcome to Flutter'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.edit),
+          ),
+        ],
+      ),
+      body: const Center(
         child: AspectRatio(
           aspectRatio: 1.0,
           child: ImmutableWidget(),
+        ),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.lightBlue,
+          child: const Center(
+            child: Text(
+              "I'm a Drawer!",
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
